1c36aef3bae280fba24e6426aa7da79caefb19cd
SPMMER <85673155+SPMMER@users.noreply.github.com>
2022-06-08 17:07:59 +0200

    modifications necessary to work with MathFX

--- goettingen/matlab/cfs2NWBChris.m
+++ goettingen/matlab/cfs2NWBChris.m
@@ -1,23 +1,23 @@
 
 
 % for debugging
-mainfolder = 'F:\uni\nebenjob\data\NPI Neuroanatomy - Raw Data CFS-Files\Eisenherz_01032022\Jenifer_20220301';
-outputfolder = 'F:\uni\nebenjob\output\cfs';
+% mainfolder = 'F:\uni\nebenjob\data\NPI Neuroanatomy - Raw Data CFS-Files\Eisenherz_01032022\Jenifer_20220301';
+% outputfolder = 'F:\uni\nebenjob\output\cfs';
 
-mainfolder = uigetdir('','Select main folder containing all cell folders'); % select individual folders at start
+mainfolder = uigetdir('E:\Data\MonkeyData\Monkeys','Select main folder containing all cell folders'); % select individual folders at start
 outputfolder = uigetdir(mainfolder,'Select output folder'); 
 
-%for debugging
-desc = struct;
-desc.number = '1';
-desc.patcher = 'T';
-desc.Amp = 'T';
-% capitalizes first letter
-desc.name = 'Test'; 
-desc.age ='1y';
-desc.sex = 'T';
-% capitalizes first letter
-desc.species = 'T'; 
+% %for debugging
+% desc = struct;
+% desc.number = '1';
+% desc.patcher = 'T';
+% desc.Amp = 'T';
+% % capitalizes first letter
+% desc.name = 'Test'; 
+% desc.age ='1y';
+% desc.sex = 'T';
+% % capitalizes first letter
+% desc.species = 'T'; 
 
 desc = getAnimalDesc();
 
